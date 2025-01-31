cf9e07fd60881876a7f74255e20ad70b20be2f6b
lawraisins <97999866+lawraisins@users.noreply.github.com>
2025-01-31 15:21:17 +0800

    Initial commit

---
+++ src/main/java/com/hcltech/BudgetbountySpringApplication.java
@@ -0,0 +1,13 @@
+package com.hcltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetbountySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetbountySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcltech/BudgetbountySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcltech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetbountySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
