bcbdc93fddf14085dbd7708ab052a02426c30729
Sumair Hasan <sumairhasan99@gmail.com>
2025-02-06 16:33:07 +0800

    added cross origin to all controllers

--- src/main/java/com/hcltech/controllers/BillController.java
+++ src/main/java/com/hcltech/controllers/BillController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // Enable CORS for React frontend
 @RequestMapping("/bills")
 public class BillController {
 
